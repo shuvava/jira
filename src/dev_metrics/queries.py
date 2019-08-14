3d61909bf9474c5b0724aad066c1d597b0982a0d
Vladimir Shurygin <vshurygin@paybyphone.com>
2019-08-14 13:32:52 -0700

    add stat calc

--- src/dev_metrics/queries.py
+++ src/dev_metrics/queries.py
@@ -3,6 +3,24 @@ from  jira import JIRA
 
 from .pass_mng import getuser, get_jira_password
 
+# get filed ids and meanings /rest/api/2/field
+# {
+#     "id": "customfield_10002",
+#     "name": "Story Points",
+#     "custom": true,
+#     "orderable": true,
+#     "navigable": true,
+#     "searchable": true,
+#     "clauseNames": [
+#         "cf[10002]",
+#         "Story Points"
+#     ],
+#     "schema": {
+#         "type": "number",
+#         "custom": "com.atlassian.jira.plugin.system.customfieldtypes:float",
+#         "customId": 10002
+#     }
+# }
 class JiraApi:
     def __init__(self, url):
         usr = getuser()
@@ -15,14 +33,23 @@ class JiraApi:
     def get_statuses(self):
         return self.jira.statuses()
 
-    def get_all_issues_for_project(self, project_key, fields='id,key,status,summary'):
+    def get_all_issues_for_project(self, project_key, fields='id,key,status,summary,customfield_10002', expand='changelog'):
         result = []
         current = 0
         total = 1
         while current<total:
             #
-            items = self.jira.search_issues(f'project={project_key} order by id', fields=fields, expand='changelog' , maxResults=50, startAt=current)
+            items = self.jira.search_issues(
+                f'project={project_key} order by id',
+                fields=fields,
+                expand=expand ,
+                maxResults=50,
+                startAt=current,
+                )
             total = items.total
             current += len(items)
             result.extend(items)
         return result
+
+    def get_issue(self, issue_id, fields='id,key,status,summary,customfield_10002', expand='changelog'):
+        return self.jira.issue(issue_id, fields=fields, expand=expand)
--- src/run.py
+++ src/run.py
@@ -1,25 +1,104 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from datetime import datetime
+from statistics import mean, median, median_grouped, mode
+from itertools import groupby
+from operator import itemgetter
+
 from dev_metrics.pass_mng import getuser, get_jira_password, remove_jira_password
 from dev_metrics import JiraApi
 
 
 base_url = 'https://ira_host'
 
+
+jra = JiraApi(base_url)
+
 def get_all_project():
-    jra = JiraApi(base_url)
     prjs = jra.get_projects()
     for prj in prjs:
         print(f'name={prj.name} key={prj.key} id={prj.id}')
 
 def get_all_issues_for_prj():
-    jra = JiraApi(base_url)
     issues = jra.get_all_issues_for_project('PAYPROC')
     print(f'cnt={len(issues)}')
+
+def get_issue_details():
+    issue = jra.get_issue('PAYPROC-277')
+    history = [(h.created, i.fromString, i.toString) for h in issue.changelog.histories for i in h.items if i.field == 'status']
+    print(f'id={issue.id} key={issue.key}')
+    now = datetime.now()
+    for h in history:
+        print(h)
+    created_dt=min(history)[0]
+    created_dt = datetime.strptime(created_dt, "%Y-%m-%dT%H:%M:%S.%f+0000")
+    dev_dt= next((i[0] for i in history if i[2]=='In Development'), created_dt)
+    if  isinstance(dev_dt, str):
+        dev_dt = datetime.strptime(dev_dt, "%Y-%m-%dT%H:%M:%S.%f+0000")
+    last_dt = next((i[0] for i in history if i[2]=='Closed'), now)
+    if  isinstance(last_dt, str):
+        last_dt = datetime.strptime(last_dt, "%Y-%m-%dT%H:%M:%S.%f+0000")
+    time_to_dev=dev_dt-created_dt
+    time_to_dep=last_dt-dev_dt
+    print(f'created_dt={created_dt} dev_dt={dev_dt} last_dt={last_dt}')
+    print(f'time_to_dev={time_to_dev.days} time_to_dep={time_to_dep.days}')
+
+def avg(lst):
+    if len(lst)>0:
+        return sum(lst) / len(lst)
+    return 0
+
+def get_team_stats_lead_time(team_name='PAYPROC'):
+    issues = jra.get_all_issues_for_project(team_name)
+    time_to_dev = []
+    time_to_dep = []
+    story_points = []
+    now = datetime.now()
+    now_week = now.isocalendar()[1]
+    for issue in issues:
+        history = [(h.created, i.fromString, i.toString) for h in issue.changelog.histories for i in h.items if i.field == 'status']
+        if len(history) > 0:
+            created_dt=min(history)[0]
+            created_dt = datetime.strptime(created_dt, "%Y-%m-%dT%H:%M:%S.%f+0000")
+            dev_dt= next((i[0] for i in history if i[2]=='In Development'), created_dt)
+            if  isinstance(dev_dt, str):
+                dev_dt = datetime.strptime(dev_dt, "%Y-%m-%dT%H:%M:%S.%f+0000")
+            last_dt = next((i[0] for i in history if i[2]=='Closed'), now)
+            if  isinstance(last_dt, str):
+                last_dt = datetime.strptime(last_dt, "%Y-%m-%dT%H:%M:%S.%f+0000")
+            _time_to_dev=dev_dt-created_dt
+            _time_to_dep=last_dt-dev_dt
+            time_to_dev.append(_time_to_dev.days)
+            time_to_dep.append(_time_to_dep.days)
+            yr = last_dt.isocalendar()[0]
+            week = last_dt.isocalendar()[1]
+            sp = 0
+            if hasattr(issue.fields, 'customfield_10002'):
+                sp = issue.fields.customfield_10002
+            story_points.append((yr, week, sp))
+    if len(time_to_dev)>0:
+        print(f'    time to development stats avg={avg(time_to_dev):.2f} mean={mean(time_to_dev):.2f} median={median(time_to_dev):.2f}')
+    if len(time_to_dep)>0:
+        print(f'    time to deployment  stats avg={avg(time_to_dep):.2f} mean={mean(time_to_dep):.2f} median={median(time_to_dep):.2f}')
+    print('\n    story point stats')
+    story_points = [i for i in story_points if i[2] is not None]
+    story_points = sorted(story_points, key= lambda d: (d[0], d[1]))
+    groups = groupby(story_points, key= lambda d: (d[0], d[1]))
+    for key, data in groups:
+        sp = [i[2] for i in data if i[2] is not None and i[0]==2019 and i[1] != now_week]
+        if len(sp)> 0:
+            print(f'        key={key} story points={sum(sp)}')
+
+def stat_for_all_teams():
+    prjs = jra.get_projects()
+    for prj in prjs:
+        print(f'team {prj.key}')
+        get_team_stats_lead_time(prj.key)
 
 if __name__ == "__main__":
     usr = getuser()
     print(usr)
-    get_all_issues_for_prj()
+    #get_all_project()
+    stat_for_all_teams()
     print('done')
